a8357c88fe8c8ee419641d10e450f4e9739bde45
Brenton Otis <36424273+brentonotis@users.noreply.github.com>
2018-06-20 22:54:24 -0500

    Update fbpi.py

--- fbpi.py
+++ fbpi.py
@@ -1,14 +1,7 @@
-hits = {'Brenton':20,
-        'Cameron':14,
-        'Justin':11}
-
-runs = {'Brenton':15,
-        'Cameron':16,
-        'Justin':10}
-
-era = {'Brenton':3.99,
-       'Cameron':2.22,
-       'Justin':1.03}
+stats = {}
+stats ['Brenton'] = {'Hits': 20, 'Runs': 19, 'ERA': 1.99}
+stats ['Justin'] = {'Hits': 19, 'Runs': 18, 'ERA': 3.01}
+stats ['Cameron'] = {'Hits': 17, 'Runs': 20, 'ERA': 1.09}
 
 hits_rank = sorted(hits, key = lambda x : hits[x], reverse = True)
 print('Hits Rank: %s' % hits_rank)
@@ -19,8 +12,3 @@ print('Runs Rank: %s' % runs_rank)
 era_rank = sorted(era, key = lambda x : era[x])
 print('ERA Rank: %s' % era_rank)
 
-''' Output sorts as intended, now to evaluate compared with each dict.
-Thinking I should be iterating through new lists and assigning value based on position in list (i.e.: first = 1, second 2, etc.)
-Then averaging aggregate of all value ranks for each category to obtain true 'power rank'
-Will need to assign value based on position in new dicts/ key/value pairs?
-'''
