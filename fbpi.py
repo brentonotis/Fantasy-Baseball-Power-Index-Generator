4c07ac4fbf27a7ea75e86e174cb7e08865bf3fa8
Brenton Otis <36424273+brentonotis@users.noreply.github.com>
2018-06-21 17:47:52 -0500

    Update fbpi.py
    Using simple dictionary per category as data structure

--- fbpi.py
+++ fbpi.py
@@ -1,8 +1,8 @@
 from collections import Counter
-stats = {}
-stats ['Brenton'] = {'Hits': 20, 'Runs': 19, 'ERA': 1.99}
-stats ['Justin'] = {'Hits': 19, 'Runs': 18, 'ERA': 3.01}
-stats ['Cameron'] = {'Hits': 17, 'Runs': 20, 'ERA': 1.09}
+
+hits = {'Brenton':20, 'Cameron':19, 'Justin':18}
+runs = {'Brenton':19, 'Cameron':20, 'Justin':18}
+era = {'Brenton':1.18, 'Cameron':1.11, 'Justin':3.12}
 
 hits_rank = sorted(hits, key = lambda x : hits[x], reverse = True)
 print('Hits Rank: %s' % hits_rank)
