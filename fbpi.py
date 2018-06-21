d1025892b36a389b856ef61bac02eca2063956f1
Brenton Otis <36424273+brentonotis@users.noreply.github.com>
2018-06-21 17:49:06 -0500

    Update fbpi.py
    sort and enumerate by category/dict

--- fbpi.py
+++ fbpi.py
@@ -4,13 +4,18 @@ hits = {'Brenton':20, 'Cameron':19, 'Justin':18}
 runs = {'Brenton':19, 'Cameron':20, 'Justin':18}
 era = {'Brenton':1.18, 'Cameron':1.11, 'Justin':3.12}
 
-hits_rank = sorted(hits, key = lambda x : hits[x], reverse = True)
-print('Hits Rank: %s' % hits_rank)
+# sort hits from most to least
+hits_rank = sorted(hits, key=hits.get, reverse=True)
 
-runs_rank = sorted(runs, key = lambda x : runs[x], reverse = True)
-print('Runs Rank: %s' % runs_rank)
+# enumerate sorted hits to number them
+hits_rank = {key: rank for rank, key in enumerate(sorted(hits, key=hits.get, reverse=True), 1)}
 
-era_rank = sorted(era, key = lambda x : era[x])
-print('ERA Rank: %s' % era_rank)
+print('Hits Rank:', hits_rank)
 
-# Going to take an approach using nested dictionary
+runs_rank = sorted(runs, key=runs.get, reverse=True)
+runs_rank = {key: rank for rank, key in enumerate(sorted(runs, key=runs.get, reverse=True), 1)}
+print('Runs Rank:', runs_rank)
+
+era_rank = sorted(era, key=era.get)
+era_rank = {key: rank for rank, key in enumerate(sorted(era, key=era.get), 1)}
+print('ERA Rank:', era_rank)
