975d752c4e0d7e881f87dc80dff3a806abc14410
Brenton Otis <36424273+brentonotis@users.noreply.github.com>
2018-10-26 16:25:28 -0500

    Update/expand Fantasy_Power_Index.java
    Added first half of full data-set; Implemented sort/printMap helper methods and added index value to sorted map;
    Trying to figure out how to pull index value from each sort per key (right now can only print it);
    Want to add index values per sort/per key and divide by total number of categories

--- Fantasy_Power_Index.java
+++ Fantasy_Power_Index.java
@@ -1,46 +1,90 @@
 package fbpi;
+
+/**
+ * Program that takes in fantasy sports data by category and returns an overall 'power rank'
+ * Takes each category, sorts by asc or desc order depending on category, enumerates/places numerical value for each user per category,
+ * combines each numerical value per category, divides by total number of categories, sorts by numerical ranking value - returns 'power rank'
+ * @author Brenton Otis
+ */
+
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class Fantasy_Power_Index {
 	
-	//set boolean for valueSort direction
 	private static boolean ASC = true;
 	private static boolean DESC = false;
 	
+	// Driver
 	public static void main (String args[]) {
 		
 		System.out.println("testing...");
 		
-		//Initialize new HashMap
-		Map<String, Integer> stats = new HashMap<>();
+		Map<String, Integer> runs = new HashMap<>(); Map<String, Integer> doubles = new HashMap<>(); Map<String, Integer> triples = new HashMap<>(); Map<String, Integer> hr = new HashMap<>(); Map<String, Integer> rbi = new HashMap<>(); Map<String, Integer> sb = new HashMap<>(); Map<String, Double> avg = new HashMap<>();
 		
-		stats.put("Brenton", 100);
-		stats.put("Cameron", 101);
-		stats.put("Justin", 99);
+		runs.put("Aaron", 469); runs.put("Michael", 478); runs.put("AustinS", 511); runs.put("MattV", 464); runs.put("Cameron", 477); runs.put("Brenton", 436); runs.put("AustinM", 441); runs.put("Luke",  450); runs.put("MattM",  455); runs.put("Evan", 443); runs.put("Cory",  470); runs.put("Justin",  458);
+		doubles.put("Aaron", 197); doubles.put("Michael", 190); doubles.put("AustinS", 178); doubles.put("MattV", 174); doubles.put("Cameron", 179); doubles.put("Brenton", 152); doubles.put("AustinM", 152); doubles.put("Luke",  195); doubles.put("MattM",  187); doubles.put("Evan", 139); doubles.put("Cory",  236); doubles.put("Justin",  183);
+		triples.put("Aaron", 20); triples.put("Michael", 12); triples.put("AustinS", 15); triples.put("MattV", 18); triples.put("Cameron", 21); triples.put("Brenton", 21); triples.put("AustinM", 13); triples.put("Luke",  19); triples.put("MattM",  13); triples.put("Evan", 8); triples.put("Cory",  9); triples.put("Justin",  20);
+		hr.put("Aaron", 129); hr.put("Michael", 118); hr.put("AustinS", 132); hr.put("MattV", 120); hr.put("Cameron", 131); hr.put("Brenton", 142); hr.put("AustinM", 149); hr.put("Luke",  148); hr.put("MattM",  140); hr.put("Evan", 128); hr.put("Cory",  125); hr.put("Justin",  114);
+		rbi.put("Aaron", 425); rbi.put("Michael", 438); rbi.put("AustinS", 512); rbi.put("MattV", 436); rbi.put("Cameron", 431); rbi.put("Brenton", 404); rbi.put("AustinM", 498); rbi.put("Luke",  453); rbi.put("MattM", 417); rbi.put("Evan", 409); rbi.put("Cory", 471); rbi.put("Justin", 429);
+		sb.put("Aaron", 42); sb.put("Michael", 62); sb.put("AustinS", 59); sb.put("MattV", 67); sb.put("Cameron", 77); sb.put("Brenton", 60); sb.put("AustinM", 46); sb.put("Luke", 33); sb.put("MattM", 82); sb.put("Evan", 32); sb.put("Cory",  35); sb.put("Justin", 47);
+		avg.put("Aaron", .2624); avg.put("Michael", .2540); avg.put("AustinS", .2650); avg.put("MattV", .2584); avg.put("Cameron", .2625); avg.put("Brenton", .2395); avg.put("AustinM", .2493); avg.put("Luke", .2592); avg.put("MattM", .2589); avg.put("Evan", .2527); avg.put("Cory", .2653); avg.put("Justin", .2589);
 		
-		System.out.println("Unsorted: ");
-		printMap(stats);
+		Map<String, Integer> sortRuns = valueSort(runs, DESC);
+		System.out.println("Sorted Runs: ");
+		System.out.println("-----------");
+		printMap(sortRuns);
 		
-		//Prints key/values sorted in ascending order
-		System.out.println("Sorted by ascending value: ");
-		Map<String, Integer> sortAsc = valueSort(stats, ASC);
-		printMap(sortAsc);
+		Map<String, Integer> sortDoubles = valueSort(doubles, DESC);
+		System.out.println("Sorted Doubles: ");
+		System.out.println("--------------");
+		printMap(sortDoubles);
 		
-		//Prints key/values in descending order
-		System.out.println("Sorted by descending value: ");
-		Map<String, Integer> sortDesc = valueSort(stats, DESC);
-		printMap(sortDesc);
+		Map<String, Integer> sortTriples = valueSort(triples, DESC);
+		System.out.println("Sorted Triples: ");
+		System.out.println("--------------");
+		printMap(sortTriples);
 		
+		Map<String, Integer> sortHr = valueSort(hr, DESC);
+		System.out.println("Sorted HR's: ");
+		System.out.println("-----------");
+		printMap(sortHr);
+		
+		Map<String, Integer> sortRbi = valueSort(rbi, DESC);
+		System.out.println("Sorted RBI's: ");
+		System.out.println("------------");
+		printMap(sortRbi);
+		
+		Map<String, Integer> sortSb = valueSort(sb, DESC);
+		System.out.println("Sorted SB's: ");
+		System.out.println("-----------");
+		printMap(sortSb);
+		
+		Map<String, Double> sortAvg = valueSortDoubs(avg, DESC);
+		System.out.println("Sorted Avg: ");
+		System.out.println("----------");
+		printMapDoubs(sortAvg);		
 	}
-		
+	
+	/**
+	 * Helper method, iterates through map and prints key/value pairs, creates index for each key
+	 * @param map - map to print
+	 */
 	public static void printMap(Map<String, Integer> map) {
-		// iterates through data and prints key/value pairs
-		map.forEach((key, value) -> System.out.println("Key: " + key + " Value: " + value)); 
-	
+		
+		int index = 1;
+		for (Map.Entry<String, Integer> entry : map.entrySet()){
+			System.out.println(index + " " + entry.getKey() + ": " + entry.getValue());
+			index++;
+ 		}		
 	}
-	
+	/**
+	 * Helper method, Sorts each statistical category from lowest-highest or vice versa
+	 * @param stats - statistical category being evaluated
+	 * @param order - ascending or descending order to be sorted (ASC/DESC variables)
+	 * @return - LinkedHashMap with key/value pairs in sorted order
+	 */
 	public static Map<String, Integer> valueSort(Map<String, Integer> stats, final boolean order) {
 		
 		List<Entry<String, Integer>> list = new LinkedList<>(stats.entrySet());
@@ -50,8 +94,29 @@ public class Fantasy_Power_Index {
 				: o1.getValue().compareTo(o2.getValue()) : o2.getValue().compareTo(o1.getValue()) == 0
 				? o2.getKey().compareTo(o1.getKey())
 				: o2.getValue().compareTo(o1.getValue()));
-		return list.stream().collect(Collectors.toMap(Entry::getKey,  Entry::getValue, (a, b) -> b, LinkedHashMap::new));
-				
-	}	
+		return list.stream().collect(Collectors.toMap(Entry::getKey,  Entry::getValue, (a, b) -> b, LinkedHashMap::new));				
+	}
 	
+	// Identical to above valueSort helper, but allows for sorting Doubles instead of integers (for ba/era/whip/knine categories)
+	public static Map<String, Double> valueSortDoubs(Map<String, Double> stats, final boolean order) {
+		
+		List<Entry<String, Double>> list = new LinkedList<>(stats.entrySet());
+		
+		list.sort((o1, o2) -> order ? o1.getValue().compareTo(o2.getValue()) == 0
+				? o1.getKey().compareTo(o2.getKey())
+				: o1.getValue().compareTo(o2.getValue()) : o2.getValue().compareTo(o1.getValue()) == 0
+				? o2.getKey().compareTo(o1.getKey())
+				: o2.getValue().compareTo(o1.getValue()));
+		return list.stream().collect(Collectors.toMap(Entry::getKey,  Entry::getValue, (a, b) -> b, LinkedHashMap::new));				
+	}
+	
+	// Identical to above printMap helper, but allows for printing Doubles instead of integers (for ba/era/whip/knine categories)
+	public static void printMapDoubs(Map<String, Double> map) {
+		
+		int index = 1;
+		for (Map.Entry<String, Double> entry : map.entrySet()){
+			System.out.println(index + " " + entry.getKey() + ": " + entry.getValue());
+			index++;
+		}		
+	}
 }
